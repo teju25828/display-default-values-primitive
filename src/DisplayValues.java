259e1a69f08d1f21b356cc48d3f40a402c086b1f
Raaj <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>
2022-09-21 17:43:08 +0530

    Final Commit

---
+++ src/DisplayValues.java
@@ -0,0 +1,22 @@
+public class DisplayValues {
+    static int a;
+    static short b;
+    static byte c;
+    static long d;
+    static float e;
+    static double f;
+    static char g;
+    static boolean h;
+
+    public static void main(String[] args) {
+
+        System.out.println("Default value of int --> " + a);
+        System.out.println("Default value of short --> " + b);
+        System.out.println("Default value of byte --> " + c);
+        System.out.println("Default value of long --> " + d);
+        System.out.println("Default value of float --> " + e);
+        System.out.println("Default value of double --> " + f);
+        System.out.println("Default value of char --> " + g);
+        System.out.println("Default value of boolean --> " + h);
+    }
+}
